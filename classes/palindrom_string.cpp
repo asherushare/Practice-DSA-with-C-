9e556669e6ff17e7a4103dc11ea8daa32450037e
Patra Kirsani <patrakirsani.234ca035@nitk.edu.in>
2024-01-16 08:22:08 +0530

    new commit

--- classes/palindrom_string.cpp
+++ classes/palindrom_string.cpp
@@ -1,29 +1,29 @@
-#include <iostream>
-using namespace std;
+// #include <iostream>
+// using namespace std;
 
-bool isPalindrome(string text) {
-  for(int i = 0; i < text.length() / 2; i++) {
-    if(text[i] != text[text.length() - i - 1]) {
-      return false;
-    }
-    else {
-      return true;
-    }
-  }
-}
+// bool isPalindrome(string text) {
+//   for(int i = 0; i < text.length() / 2; i++) {
+//     if(text[i] != text[text.length() - i - 1]) {
+//       return false;
+//     }
+//     else {
+//       return true;
+//     }
+//   }
+// }
 
-int main() {
+// int main() {
 
-  string name;
-  cout << "Enter string here: ";
-  getline(cin, name);
+//   string name;
+//   cout << "Enter string here: ";
+//   getline(cin, name);
 
-  if(isPalindrome(name)) {
-    cout << "This is a palindrome";
-  }
-  else {
-    cout << "not a palindrome";
-  }
+//   if(isPalindrome(name)) {
+//     cout << "This is a palindrome";
+//   }
+//   else {
+//     cout << "not a palindrome";
+//   }
 
-  return 0;
-}
+//   return 0;
+// }
