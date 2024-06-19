09f77194959ade24b41cfcf005a4ce7d79095c9d
Patra Kirsani <patrakirsani.234ca035@nitk.edu.in>
2024-06-19 11:44:38 +0530

    New code added

--- Sorting/2_selection_sort.cpp
+++ Sorting/2_selection_sort.cpp
@@ -1,68 +1,35 @@
-// #include <iostream>
-// using namespace std;
-
-// int main() {
-
-//   int size;
-//   cout << ("Enter size of the array: ");
-//   cin >> size;
-
-
-//   int arr[100]; 
-
-//   cout << ("Enter ") << size << (" elements here: ");
-//   for(int i = 0; i < size; i++) {
-//     cin >> arr[i];
-//   }
-
-//   for(int i = 0; i < size-1; i++) {
-//     int min = i;
-//     for(int j = i+1; j< n; j++) {
-//       if(a[])
-//     }
-//   }
-
-//   return 0;
-// }
-
 #include <iostream>
 using namespace std;
 
+void selectionSort(int arr[], int n) {
+  for(int i = 0; i <= n - 2; i++) {
+    int mini = i;
+    for(int j = i; j <= n - 1; j++) {
+      if(arr[j] < arr[mini]) {
+        mini = j;
+      }
+      int temp = arr[mini];
+      arr[mini] = arr[i];
+      arr[i] = temp;
+    }
+  }
+}
+
 int main() {
 
-  int size;
-  cout << "Enter the size of the array: ";
-  cin >> size;
+  int n;
+  cin >> n;
 
   int arr[100];
-  cout << "Enter " << size << " elements here: ";
-  for(int i = 0; i < size; i++) {
+  for(int i = 0; i < n; i++) {
     cin >> arr[i];
   }
 
-  cout << endl;
-  
-  cout << "Array elements are: ";
-  for(int i = 0; i < size; i++) {
+  selectionSort(arr, n);
+  for(int i = 0; i < n; i++) {
     cout << arr[i] << " ";
   }
 
-  cout << endl;
-  
-  cout << "sorting using selection sort: ";
-  for(int i = 0; i < size - 1; i++) {
-    int minIndex = i;
-    for(int j = i + 1; j < size; j++) {
-      if (arr[minIndex] > arr[j]) {
-        minIndex = j;
-      }
-    }
-    swap(arr[minIndex], arr[i]);
-  }
-
-  for(int i = 0; i < size; i++) {
-    cout << arr[i] << " ";
-  }
 
   return 0;
 }
