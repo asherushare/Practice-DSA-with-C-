75038bd1ddad3b21eacbc9f4190ff4e1d299e04a
Patra Kirsani <patrakirsani.234ca035@nitk.edu.in>
2024-06-21 11:45:54 +0530

    Insertion sort added

--- Sorting/3_insertion_sort.cpp
+++ Sorting/3_insertion_sort.cpp
@@ -1,7 +1,45 @@
 #include <iostream>
 using namespace std;
 
+void insertionSort(int arr[], int size) {
+  for(int i = 0; i < size; i++) {
+    int j = i;
+
+    while(j > 0 && arr[j - 1] > arr[j]) {
+      swap(arr[j], arr[j - 1]);
+      j--;
+    }
+  }
+}
+
 int main() {
+
+  int size;
+  int arr[100];
+
+  cout << "Enter the size of the array: ";
+  cin >> size;
+
+  cout << "Enter arra elements: " << endl;
+  for(int i = 0; i < size; i++) {
+    cin >> arr[i];
+  }
+
+  cout << endl;
+
+  cout << "Array elements: " << endl;
+  for(int i = 0; i < size; i++) {
+    cout << arr[i] << " ";
+  }
+
+  insertionSort(arr, size);
+
+  cout << endl;
+
+  cout << "Sorted elements are: " << endl;
+  for(int i = 0; i < size; i++) {
+    cout << arr[i] << " ";
+  }
 
   return 0;
 }
